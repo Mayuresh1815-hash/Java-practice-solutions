179c5cbc8b6c3162308f073445a0695ae69e4163
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-06-12 10:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.70%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -3,7 +3,8 @@ class Solution {
         int c=0;
         for(int i=0;i<nums.length;i++){
             if(val!=nums[i]){
-                nums[c++]=nums[i];
+                nums[c]=nums[i];
+              c++;
             }
         }
         return c;
