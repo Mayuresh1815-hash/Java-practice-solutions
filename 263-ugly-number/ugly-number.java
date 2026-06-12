91104ee4b4a2f1797e81f6883a3ed8ab665791c0
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-06-12 21:54:38 +0530

    Time: 1 ms (99.25%) | Memory: 42.7 MB (32.14%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,16 +1,4 @@
-// class Solution {
-//     public boolean isUgly(int n) {
-//     //     if((n%2==0)||(n%3==0)||(n%5==0)){
-//     //     return true;
-//     //     }
-//     // return false;
-//             return (n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0);
-
-//     }
-
-
-// }
-class Solution {
+ class Solution {
     public boolean isUgly(int n) {
         if (n <= 0) {
             return false;
