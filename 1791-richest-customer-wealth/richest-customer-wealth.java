c077f705b86baf3c9ce41effb43b5087a28156cf
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-06-21 22:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (94.24%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-     int max=Integer.MIN_VALUE;
-        for(int row=0;row<accounts.length;row++){
-            int sum=0;
-            for(int col=0;col<accounts[row].length;col++){
-                sum=sum+accounts[row][col];
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row < accounts.length; row++) {
+            int sum = 0;
+            for (int col = 0; col < accounts[row].length; col++) {
+                sum = sum + accounts[row][col];
             }
-             if(max<sum){
-                max=sum;
-             }  
+            if (max < sum) {
+                max = sum;
+            }
         }
-             return max;
+        return max;
     }
 }
