0063c43ed8827a5fefea9851fc44dd9dcb3e5af9
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-06-21 19:21:23 +0530

    Time: 2113 ms (12.89%) | Memory: 42 MB (80.07%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        ArrayList <Integer> list=new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
-        for(int i=1;i<num;i++){
-            if(num%i==0){
+        for(int i = 1; i < num; i++) {
+            if(num % i == 0) {
                 list.add(i);
             }
         }
-      int sum=0; 
-    for(int j=0;j<list.size();j++){
-        sum=list.get(j)+sum;
-    }
- if(sum==num){
-    return true;
- }
- else{
-    return false;
- }
 
+        int sum = 0;
+
+        for(int j = 0; j < list.size(); j++) {
+            sum = sum + list.get(j);
+        }
+
+        return sum == num;
     }
 }
