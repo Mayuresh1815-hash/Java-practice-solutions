0e0db670256d72ae0a4ac59ebeb8d931c7abb42a
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-06-22 20:08:10 +0530

    Time: 1 ms (22.59%) | Memory: 48.4 MB (20.89%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,17 +1,27 @@
 class Solution {
     public void reverseString(char[] s) {
-         swap(s,0,s.length-1);
-    }
+    //      swap(s,0,s.length-1);
+    // }
 
-    void swap(char[] s,int i,  int j){
+    // void swap(char[] s,int i,  int j){
         
-        if(i>=j){
-            return;
-        }
-            char temp =s[i];
+    //     if(i>=j){
+    //         return;
+    //     }
+    //         char temp =s[i];
+    //             s[i]=s[j];
+    //             s[j]=temp;
+    //            swap(s,i+1,j-1);
+    int i=0;
+    int j=s.length-1;
+    while(i<j){
+          char temp =s[i];
                 s[i]=s[j];
                 s[j]=temp;
-               swap(s,i+1,j-1);
+                i++;
+                j--;
+    }
+    return ;
 
         }
 
