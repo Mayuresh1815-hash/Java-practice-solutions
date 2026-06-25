7c53fb0540e197f002fc6ddde2ca62e4a0f466df
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-06-25 14:20:03 +0530

    Time: 25 ms (12.02%) | Memory: 76.9 MB (84.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,28 +1,15 @@
-import java.util.HashSet;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
+         Arrays.sort(nums);
+        for(int i = 0; i < nums.length-1; i++) {
 
-        for(int num : nums) {
-            if(set.contains(num)) {
-                return true;
-            }
-            set.add(num);
+
+                if(nums[i] == nums[i+1]) {
+                    return true;
+                }
+            
         }
-
         return false;
+
     }
 }
-// class Solution {
-//     public boolean containsDuplicate(int[] nums) {
-//         for(int i = 0; i < nums.length; i++) {
-//             for(int j = i + 1; j < nums.length; j++) {
-//                 if(nums[i] == nums[j]) {
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// }
