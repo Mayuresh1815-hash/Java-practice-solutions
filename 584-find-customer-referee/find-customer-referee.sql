5197b4691ce8866c0c6a852d9fa8877c549f275d
Mayuresh Balaso Patil <mayureshp722@gmail.com>
2026-07-02 23:43:40 +0530

    Time: 605 ms (20.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+
+ SELECT name
+FROM Customer
+WHERE referee_id != 2   OR referee_id IS NULL;
+
+
+-- select name 
+-- from Customer
+-- where referee_id !=2 OR referee_id is null;
